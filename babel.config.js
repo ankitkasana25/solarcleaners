93be85b0d450f84163f5546494cbc5988454e3e4
Ankit Kasana <154894190+ankitkasana25@users.noreply.github.com>
2025-12-13 17:48:06 +0530

    feat: update app name to "Solar Cleaning Expert" and remove `react-native-worklets` Babel plugin

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,6 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     // other plugins (if any) go here...
-
-    'react-native-worklets/plugin',
     'react-native-reanimated/plugin', // 👈 must be LAST
   ],
   env: {
